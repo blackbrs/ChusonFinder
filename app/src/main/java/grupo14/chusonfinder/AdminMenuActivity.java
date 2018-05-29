2a3dc8703eef2f36bdf614eb6bce680ebd9a23cb
blackbrs <guillermobrs@gmail.com>
2018-05-28 22:14:27 -0600

    version alpha:
    -Login con firebase (correo y facebook) funcional sin bugs hasta el momento

--- app/src/main/java/grupo14/chusonfinder/AdminMenuActivity.java
+++ app/src/main/java/grupo14/chusonfinder/AdminMenuActivity.java
@@ -1,7 +1,13 @@
 package grupo14.chusonfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminMenuActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class AdminMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+
+    }
+    private void goMainScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void Logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+        goMainScreen();
     }
 }
--- app/src/main/java/grupo14/chusonfinder/UsuarioLoginActivity.java
+++ app/src/main/java/grupo14/chusonfinder/UsuarioLoginActivity.java
@@ -9,20 +9,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class UsuarioLoginActivity extends AppCompatActivity {
     private EditText correo,contra;
     private Button log,regis;
 
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
     private FirebaseAuth nAuth;
     private FirebaseAuth.AuthStateListener firebaseAutListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,7 +106,52 @@ public class UsuarioLoginActivity extends AppCompatActivity {
 
             }
         });
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //goMainScreen();
+                handleFacebookAccesToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.cancel_login, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),R.string.error_login,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
+    private void handleFacebookAccesToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        nAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),R.string.firebase_error_login,Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private void goMainScreen(){
+            Intent intent = new Intent(this,AdminMenuActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+    }
+
+  protected void onActivityResult(int requestCode,int resultCode,Intent data ){
+        super.onActivityResult(requestCode,resultCode,data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+  }
 
     @Override
     protected void onStart() {
