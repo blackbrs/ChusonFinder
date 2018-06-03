67420ca4f7b406271e88d9aa0bef9901d01c4ae9
Mego1596 <maurigranados1596@gmail.com>
2018-06-02 22:40:47 -0600

    Crear un Post

--- app/src/main/java/grupo14/chusonfinder/BlogActivity.java
+++ app/src/main/java/grupo14/chusonfinder/BlogActivity.java
@@ -1,7 +1,10 @@
 package grupo14.chusonfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class BlogActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class BlogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== R.id.action_add){
+        startActivity(new Intent(BlogActivity.this,PostActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
