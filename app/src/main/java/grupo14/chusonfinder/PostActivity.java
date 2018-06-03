750bdd0e5303e56325eb1714c15bad6d7a2d5b4e
Mego1596 <maurigranados1596@gmail.com>
2018-06-02 23:51:11 -0600

    GUARDAR IMAGEN POST

--- app/src/main/java/grupo14/chusonfinder/PostActivity.java
+++ app/src/main/java/grupo14/chusonfinder/PostActivity.java
@@ -1,13 +1,89 @@
 package grupo14.chusonfinder;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectedImage;
+    private static final int GALLERY_REQUEST=1;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitBtn;
+    private Uri mImageUri=null;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectedImage =(ImageButton) findViewById(R.id.imageSelected);
+        mPostTitle = (EditText) findViewById(R.id.titleField);
+        mPostDesc = (EditText) findViewById(R.id.descField);
+        mSubmitBtn = (Button) findViewById(R.id.submitBtn);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mProgress = new ProgressDialog(this);
+        mSelectedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_REQUEST);
+            }
+        });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to Blog...");
+        mProgress.show();
+        String title_val=mPostTitle.getText().toString().trim();
+        String desc_val=mPostDesc.getText().toString().trim();
+        if(!TextUtils.isEmpty(title_val)&& !TextUtils.isEmpty(desc_val)&& mImageUri!=null){
+            StorageReference filepath= mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getStorage().getDownloadUrl().getResult();
+                    mProgress.dismiss();
+                }
+            });
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GALLERY_REQUEST && resultCode==RESULT_OK){
+
+            mImageUri = data.getData();
+            mSelectedImage.setImageURI(mImageUri);
+
+        }
+
     }
 }
