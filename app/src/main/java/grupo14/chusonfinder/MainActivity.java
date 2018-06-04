0138bc33db7985ce3da5ebd0b9377aa13f560980
blackbrs <guillermobrs@gmail.com>
2018-06-03 19:26:54 -0600

    App En un 95% terminada

--- app/src/main/java/grupo14/chusonfinder/MainActivity.java
+++ app/src/main/java/grupo14/chusonfinder/MainActivity.java
@@ -1,6 +1,10 @@
 package grupo14.chusonfinder;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +13,43 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button administrador,user;
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    int accion=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         administrador = (Button)findViewById(R.id.admin);
         user = (Button)findViewById(R.id.usuario);
+        administrador.setVisibility(View.INVISIBLE);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor == null){ finish(); }
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float x = sensorEvent.values[0];
+                if(x<-5){
+                        accion++;
+                        administrador.setVisibility(View.VISIBLE);
+                        onResume();
+                }else if(x>5){
+                        accion++;
+                    administrador.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
 
         administrador.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
         user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +71,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void start(){
+        sensorManager.registerListener(sensorEventListener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+private void stop(){
+
+}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        start();
+        super.onResume();
     }
 }
