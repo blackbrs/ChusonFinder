f246c7d031ea86f36a462cdeb7fba86f18b792e0
Walter Luna <mywalter97@gmail.com>
2018-06-13 16:25:23 -0600

    Agregado Speech y txtToSpeech

--- app/src/main/java/grupo14/chusonfinder/PostActivity.java
+++ app/src/main/java/grupo14/chusonfinder/PostActivity.java
@@ -3,13 +3,20 @@ package grupo14.chusonfinder;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
@@ -18,17 +25,20 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-public class PostActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
 
-    private ImageButton mSelectedImage;
-    private static final int GALLERY_REQUEST=1;
+public class PostActivity extends AppCompatActivity implements OnClickListener {
+
     private EditText mPostTitle;
     private EditText mPostDesc;
     private Button mSubmitBtn;
-    private Uri mImageUri=null;
     private StorageReference mStorage;
     private ProgressDialog mProgress;
     private DatabaseReference mDatabase;
+    private Button btnSpeech, btnTxtspch;
+    private TextToSpeech tts;
+    int check = 1111;
 
 
     @Override
@@ -48,7 +58,25 @@ public class PostActivity extends AppCompatActivity {
                 startPosting();
             }
         });
+        btnSpeech = (Button)  findViewById(R.id.spchBtn);
+        btnTxtspch = (Button) findViewById(R.id.txtspchBtn);
+        tts = new TextToSpeech(this,OnInit);
+        btnSpeech.setOnClickListener(this);
+        btnTxtspch.setOnClickListener(this);
     }
+
+    TextToSpeech.OnInitListener OnInit = new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+            if (TextToSpeech.SUCCESS==status){
+                tts.setLanguage(new Locale("spa","ESP"));
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "TTS no disponible",Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     private void startPosting() {
         mProgress.setMessage("Chuzoneando :v...");
@@ -56,25 +84,40 @@ public class PostActivity extends AppCompatActivity {
         mProgress.show();
         final String title_val=mPostTitle.getText().toString().trim();
         final String desc_val=mPostDesc.getText().toString().trim();
-                    mProgress.dismiss();
-                    DatabaseReference newPost = mDatabase.push();
-                    newPost.child("title").setValue(title_val);
-                    newPost.child("desc").setValue(desc_val);
-                    startActivity(new Intent(PostActivity.this,BlogActivity.class));
+        mProgress.dismiss();
+        DatabaseReference newPost = mDatabase.push();
+        newPost.child("title").setValue(title_val);
+        newPost.child("desc").setValue(desc_val);
+        startActivity(new Intent(PostActivity.this,BlogActivity.class));
 
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode==GALLERY_REQUEST && resultCode==RESULT_OK){
-
-            mImageUri = data.getData();
-            mSelectedImage.setImageURI(mImageUri);
-
+        if (requestCode==check && resultCode==RESULT_OK){
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String voz = results.get(0).toString();
+            mPostDesc.setText(voz);
         }
-
+        super.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.spchBtn) {
+            Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hable ahora ");
+            startActivityForResult(i, check);
+        }
+        if (v.getId()==R.id.txtspchBtn){
+            tts.speak(mPostDesc.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+        }
+    }
+
+    public void onDestroy(){
+        tts.shutdown();
+        super.onDestroy();
+    }
+
 }
